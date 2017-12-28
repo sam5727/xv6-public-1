f9f9fd00be14507d8812181eb6409a2c2635dad9
YoCheng <xyox860224@hotmail.com>
2017-12-28 11:40:17 +0800

    date add time area

--- date.c
+++ date.c
@@ -11,12 +11,43 @@ int main(int argc, char *argv[])
 		printf(2, "date failed\n");
 		exit();
 	}
-	r.hour += 8;
-	if(r.hour >= 24){
-		r.hour -= 24;
-		r.day += 1;
-	}
 
+
+
+	if(argc == 2)
+	{
+		if(atoi(argv[1]) > 12 || atoi(argv[1]) <-12)
+		{
+			printf(2, "TimeArea Error\n");
+			exit();
+		}
+		
+		int newHour = r.hour + atoi(argv[1]);
+
+		if(newHour >= 24)
+		{
+			r.hour = newHour - 24;
+			r.day += 1;
+		}
+		else if(newHour < 0)
+		{	
+			r.hour = r.hour + 24 - atoi(argv[1]);
+			r.day -= 1;
+		}
+		else
+		{
+			r.hour = newHour;
+		}
+	}
+	else
+	{
+		r.hour += 8;
+		if(r.hour >= 24)
+		{
+			r.hour -= 24;
+			r.day += 1;
+		}
+	}
 	printf(1, "The time is: ");
 	pulseZero(r.year, "/");
 	pulseZero(r.month, "/");
